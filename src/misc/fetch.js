cc01d2e50753dd484e993f84de50190eeae98604
Victor Glindås <glinkis@gmail.com>
2018-10-21 12:33:01 +0200

    Document fetch  utility.

--- src/misc/fetch.js
+++ src/misc/fetch.js
@@ -1,11 +1,19 @@
 import api from "../../trial-files/softrobot_test_api.min.js";
 
+/**
+ * Utility function to allow for async requests.
+ * @param {string} method - Request method, e.g. GET, POST, & DELETE.
+ * @param {string} url - Url to send the request.
+ * @param  {...any} params - Miscellaneous parameters to pass with the request.
+ * @returns {Promise<string>} - A promise that resolves to a json string.
+ */
 const fetch = (method, url, ...params) =>
   new Promise((resolve, reject) => {
     const xhr = api.XMLHttpRequest();
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+          console.log(xhr.responseText);
           resolve(xhr.responseText);
         } else {
           reject();
