314096d91f882607837da59f196767cc821ba150
Victor Glindås <glinkis@gmail.com>
2018-10-21 11:47:35 +0200

    Extract css on build.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,17 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { common } = require("./webpack.common");
+const { common, cssLoader } = require("./webpack.common");
+
+cssLoader.use = [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"];
 
 module.exports = {
   ...common,
   mode: "production",
   plugins: [
+    new MiniCssExtractPlugin({
+      chunkFilename: "[id].css",
+      filename: "[name].css"
+    }),
     new HtmlWebpackPlugin({
       title: "work-trial-softrobot",
       template: `${__dirname}/template.prod.html`,
