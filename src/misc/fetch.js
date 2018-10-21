22ca072ecc12b076e650721f539bd0386ccd949b
Victor Glindås <glinkis@gmail.com>
2018-10-21 12:42:41 +0200

    Allow request to time out.

--- src/misc/fetch.js
+++ src/misc/fetch.js
@@ -10,6 +10,8 @@ import api from "../../trial-files/softrobot_test_api.min.js";
 const fetch = (method, url, ...params) =>
   new Promise((resolve, reject) => {
     const xhr = api.XMLHttpRequest();
+    xhr.timeout = 2000;
+    xhr.ontimeout = reject;
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
