c477b54edb4a1286207072991b96a5be5a33279f
Victor Glindås <glinkis@gmail.com>
2018-10-24 18:27:13 +0200

    Fix out of bounds edit div.

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -3,10 +3,10 @@ import "./TableHeader.scss";
 
 const TableHeader = () => (
   <div className="table-header">
-    <span>Text</span>
-    <span>Date</span>
-    <span>Owner</span>
-    <span>Status</span>
+    <span className="sorter">Text</span>
+    <span className="sorter">Date</span>
+    <span className="sorter">Owner</span>
+    <span className="sorter">Status</span>
     <span />
   </div>
 );
