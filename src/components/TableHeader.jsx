95fbcbf0661d2e938c647a675b85e37489f1b217
Victor Glindås <glinkis@gmail.com>
2018-10-24 19:33:26 +0200

    Implement sorting.

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./TableHeader.scss";
 
-const TableHeader = () => (
-  <div className="table-header">
-    <span className="sorter">Text</span>
-    <span className="sorter">Date</span>
-    <span className="sorter">Owner</span>
-    <span className="sorter">Status</span>
-    <span />
-  </div>
-);
+const TableHeader = ({ sorter, onClick }) => {
+  const activeSort = name => (sorter.name === name ? sorter.direction : "");
+  return (
+    <div className="table-header">
+      <span className={`sorter ${activeSort("Text")}`} onClick={onClick}>
+        Text
+      </span>
+      <span className={`sorter ${activeSort("Date")}`} onClick={onClick}>
+        Date
+      </span>
+      <span className={`sorter ${activeSort("Owner")}`} onClick={onClick}>
+        Owner
+      </span>
+      <span className={`sorter ${activeSort("Status")}`} onClick={onClick}>
+        Status
+      </span>
+      <span />
+    </div>
+  );
+};
 
 export default TableHeader;
--- src/containers/Table.jsx
+++ src/containers/Table.jsx
@@ -2,22 +2,82 @@ import React from "react";
 import "react-toggle/style.css";
 import { connect } from "react-redux";
 import { updateItem } from "../actions/itemActions";
+import { sortByString, sortByBoolean } from "../utils/sort";
 import TableHeader from "../components/TableHeader";
 import TableRow from "../components/TableRow";
 import "./Table.scss";
 
 @connect(state => state)
 export default class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { sort: this.defaultSort, sorter: {} };
+  }
+
   updateItem = item => {
     const { dispatch } = this.props;
     dispatch(updateItem(item));
   };
+
+  onSort = event => {
+    const { users } = this.props.request;
+    let name = event.target.textContent;
+    let sort = this.defaultSort;
+    let sorter = {};
+
+    switch (name) {
+      case "Text":
+        sort = (a, b) => sortByString(a.text, b.text);
+        sorter = { name: "Text" };
+        break;
+      case "Date":
+        sort = (a, b) => sortByString(a.date, b.date);
+        sorter = { name: "Date" };
+        break;
+      case "Owner":
+        sort = (a, b) => sortByString(users[a.userId], users[b.userId]);
+        sorter = { name: "Owner" };
+        break;
+      case "Status":
+        sort = (a, b) => sortByBoolean(a.active, b.active);
+        sorter = { name: "Status" };
+        break;
+    }
+
+    if (
+      this.state.sorter.name === name &&
+      this.state.sorter.direction === "up"
+    ) {
+      sorter.direction = "down";
+    } else if (
+      this.state.sorter.name === name &&
+      this.state.sorter.direction === "down"
+    ) {
+      sorter = {};
+      sort = this.defaultSort;
+    } else {
+      sorter.direction = "up";
+    }
+
+    this.setState({ sort, sorter });
+  };
+
+  defaultSort(a, b) {
+    return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
+  }
+
   render() {
     const { users, items } = this.props.request;
     const { updating, failed } = this.props.item;
+
+    items = items.sort(this.state.sort);
+    if (this.state.sorter.direction === "down") {
+      items = items.reverse();
+    }
+
     return (
       <div className="table">
-        {<TableHeader />}
+        {<TableHeader onClick={this.onSort} sorter={this.state.sorter} />}
         {items.map(item => (
           <TableRow
             key={item.id}
