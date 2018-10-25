4c89c77a30df22e8470a7f52d04382a8b9922129
Victor Glindås <glinkis@gmail.com>
2018-10-25 20:49:39 +0200

    Extract TableRowEditable.

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,109 +1,38 @@
 // @ts-check
 import React from "react";
-import Toggle from "react-toggle";
-import ErrorMessage from "../components/ErrorMessage";
-import getFormattedTime from "../utils/getFormattedTime";
 import "./TableRow.scss";
-
-const TextInput = ({ value, onChange }) => (
-  <textarea name="text" value={value} onChange={onChange} />
-);
-
-const UserInput = ({ users, value, onChange }) => (
-  <select name="userId" value={value} onChange={onChange}>
-    {users.map((user, i) => (
-      <option key={i} value={i}>
-        {user}
-      </option>
-    ))}
-  </select>
-);
-
-const StatusInput = ({ value, onChange }) => (
-  <span className="status">
-    <Toggle
-      name="active"
-      type="checkbox"
-      checked={!!value}
-      onChange={onChange}
-    />
-  </span>
-);
+import TableRowEditable from "./TableRowEditable";
 
 export default class TableRow extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...props.item, isEditing: false };
+    this.state = { isEditing: false };
   }
-
-  handleInputChange = ({ target }) => {
-    if (target.type === "checkbox") {
-      this.setState({ [target.name]: target.checked });
-      return;
-    }
-    this.setState({ [target.name]: target.value });
-  };
 
   onEditEnable = () => {
     this.setState({ isEditing: true });
   };
 
-  onEditCancel = () => {
-    this.setState({ isEditing: false });
-  };
-
-  onSubmit = event => {
-    event.preventDefault();
-    const { isEditing, ...itemValues } = this.state;
-    this.props.onUpdate({ ...itemValues, date: getFormattedTime() });
+  onEditDisable = () => {
     this.setState({ isEditing: false });
   };
 
   render() {
-    const { text, date, userId, active, isEditing } = this.state;
-    const { users, isUpdating, failedToUpdate } = this.props;
+    const { isEditing } = this.state;
+    const { users, item, onUpdate, isUpdating, failedToUpdate } = this.props;
+    const { text, date, userId, active } = this.props.item;
 
-    if (failedToUpdate) {
+    if (isEditing || isUpdating || failedToUpdate) {
       return (
-        <div className="table-row">
-          <ErrorMessage message={"Failed to update item."} />
-          <i onClick={this.onSubmit} className="material-icons">
-            refresh
-          </i>
-        </div>
-      );
-    }
-
-    if (isUpdating) {
-      return (
-        <div className="table-row">
-          <span className="fill">Updating item...</span>
-        </div>
-      );
-    }
-
-    if (isEditing) {
-      return (
-        <form className="table-row" onSubmit={this.onSubmit}>
-          <TextInput value={text} onChange={this.handleInputChange} />
-          <span />
-          <span />
-          <UserInput
-            users={users}
-            value={userId}
-            onChange={this.handleInputChange}
-          />
-          <StatusInput value={active} onChange={this.handleInputChange} />
-          <span className="edit">
-            <i onClick={this.onSubmit} className="material-icons">
-              save
-            </i>
-            <i onClick={this.onEditCancel} className="material-icons">
-              cancel
-            </i>
-            <button style={{ display: "none" }} type="submit" />
-          </span>
-        </form>
+        <TableRowEditable
+          users={users}
+          item={item}
+          onSubmit={this.onEditDisable}
+          onCancel={this.onEditDisable}
+          onUpdate={onUpdate}
+          isUpdating={isUpdating}
+          failedToUpdate={failedToUpdate}
+        />
       );
     }
 
---
+++ src/components/TableRowEditable.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Toggle from "react-toggle";
+import ErrorMessage from "./ErrorMessage";
+import getFormattedTime from "../utils/getFormattedTime";
+
+export default class TableRowEditable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props.item };
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    const { isEditing, ...itemValues } = this.state;
+    this.props.onUpdate({ ...itemValues, date: getFormattedTime() });
+    this.props.onSubmit();
+  };
+
+  onCancel = () => {
+    this.props.onCancel();
+  };
+
+  handleInputChange = ({ target }) => {
+    if (target.type === "checkbox") {
+      this.setState({ [target.name]: target.checked });
+      return;
+    }
+    this.setState({ [target.name]: target.value });
+  };
+
+  render() {
+    const { text, userId, active } = this.state;
+    const { users, failedToUpdate, isUpdating } = this.props;
+
+    if (failedToUpdate) {
+      return (
+        <div className="table-row">
+          <ErrorMessage message={"Failed to update item."} />
+          <i onClick={this.onSubmit} className="material-icons">
+            refresh
+          </i>
+        </div>
+      );
+    }
+
+    if (isUpdating) {
+      return (
+        <div className="table-row">
+          <span className="fill">Updating item...</span>
+        </div>
+      );
+    }
+
+    return (
+      <form className="table-row" onSubmit={this.onSubmit}>
+        <TextInput value={text} onChange={this.handleInputChange} />
+        <span />
+        <span />
+        <UserInput
+          users={users}
+          value={userId}
+          onChange={this.handleInputChange}
+        />
+        <StatusInput value={active} onChange={this.handleInputChange} />
+        <span className="edit">
+          <i onClick={this.onSubmit} className="material-icons">
+            save
+          </i>
+          <i onClick={this.onCancel} className="material-icons">
+            cancel
+          </i>
+          <button style={{ display: "none" }} type="submit" />
+        </span>
+      </form>
+    );
+  }
+}
+
+const TextInput = ({ value, onChange }) => (
+  <textarea name="text" value={value} onChange={onChange} />
+);
+
+const UserInput = ({ users, value, onChange }) => (
+  <select name="userId" value={value} onChange={onChange}>
+    {users.map((user, i) => (
+      <option key={i} value={i}>
+        {user}
+      </option>
+    ))}
+  </select>
+);
+
+const StatusInput = ({ value, onChange }) => (
+  <span className="status">
+    <Toggle
+      name="active"
+      type="checkbox"
+      checked={!!value}
+      onChange={onChange}
+    />
+  </span>
+);
