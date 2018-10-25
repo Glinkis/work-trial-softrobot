d51b3a0920c8d9c40fb3306069f2530467150d19
Victor Glindås <glinkis@gmail.com>
2018-10-25 12:36:27 +0200

    Document sort utils.

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,7 +1,19 @@
+// @ts-check
+
+/**
+ * Sort strings alphabetically.
+ * @param {string} a
+ * @param {string} b
+ */
 export const sortByString = (a, b) => {
   return a.localeCompare(b);
 };
 
+/**
+ * Sort strings booleans by truthiness.
+ * @param {boolean} a
+ * @param {boolean} b
+ */
 export const sortByBoolean = (a, b) => {
   return a === b ? 0 : a ? -1 : 1;
 };
