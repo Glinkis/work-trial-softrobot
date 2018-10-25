7c8df85264f1f762376fb5c11175786b30d1bc93
Victor Glindås <glinkis@gmail.com>
2018-10-25 16:27:55 +0200

    Remove absolute immutability from receiveItems.

--- src/reducers/requestReducer.spec.js
+++ src/reducers/requestReducer.spec.js
@@ -64,15 +64,17 @@ describe("request reducer", () => {
       openRequests: 2,
       items: []
     };
-    const item = { id: 0, text: "", date: "", userId: 0, active: false };
-    const reducedState = reducer(state, receiveItems([item]));
-
-    expect(reducedState.items[0]).to.not.equal(item);
-
-    expect(reducedState).deep.equals({
+    const item = {
+      id: 0,
+      text: "",
+      date: "",
+      userId: 0,
+      active: false
+    };
+    expect(reducer(state, receiveItems([item]))).deep.equals({
       ...defaultState,
       openRequests: 1,
-      items: [{ id: 0, text: "", date: "", userId: 0, active: false }]
+      items: [item]
     });
   });
 
