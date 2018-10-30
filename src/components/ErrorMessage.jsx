a5aa64da59fc21d167c27b2794d19076ea0ba606
Victor Glindås <glinkis@gmail.com>
2018-10-30 16:37:36 +0100

    Document ErrorMessage.

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -2,6 +2,11 @@
 import React from "react";
 import "./ErrorMessage.scss";
 
+/**
+ * Component for displaying error messages.
+ * @param {Object} props
+ * @param {string} props.message
+ */
 const ErrorMessage = ({ message }) => (
   <div className="error-message">{message}</div>
 );
