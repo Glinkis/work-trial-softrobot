68ff86725b786bf447c9a0da4c090ec20a34257e
Victor Glindås <glinkis@gmail.com>
2018-12-12 12:23:17 +0100

    Refactor action tests.

--- src/actions/tableActions.spec.js
+++ src/actions/tableActions.spec.js
@@ -4,29 +4,23 @@ import * as actions from "./tableActions";
 
 describe("table actions", () => {
   it("should create an action to sort the table", () => {
-    const action = actions.sortTable("DATE");
-    const expectedAction = {
+    expect(actions.sortTable("DATE")).deep.equals({
       type: actions.SORT_TABLE,
       payload: "DATE"
-    };
-    expect(action).deep.equals(expectedAction);
+    });
   });
 
   it("should create an action set the table rows", () => {
-    const action = actions.setTableRows(10);
-    const expectedAction = {
+    expect(actions.setTableRows(10)).deep.equals({
       type: actions.SET_TABLE_ROW,
       payload: 10
-    };
-    expect(action).deep.equals(expectedAction);
+    });
   });
 
   it("should create an action set the table page", () => {
-    const action = actions.setTablePage(1);
-    const expectedAction = {
+    expect(actions.setTablePage(1)).deep.equals({
       type: actions.SET_TABLE_PAGE,
       payload: 1
-    };
-    expect(action).deep.equals(expectedAction);
+    });
   });
 });
