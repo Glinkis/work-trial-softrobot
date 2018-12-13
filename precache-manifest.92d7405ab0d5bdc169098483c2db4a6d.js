b4640f902acedcc70e79ce170cc579393c75e940
Victor Glindås <glinkis@gmail.com>
2018-12-13 13:48:07 +0100

    Updates

---
+++ precache-manifest.92d7405ab0d5bdc169098483c2db4a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65339717ea22b8255f9a",
+    "url": "vendors~main.js"
+  },
+  {
+    "revision": "65339717ea22b8255f9a",
+    "url": "vendors~main.css"
+  },
+  {
+    "revision": "3aaa912fd37e258bb4b5",
+    "url": "runtime~main.js"
+  },
+  {
+    "revision": "e0b8f7a79831216918c9",
+    "url": "main.js"
+  },
+  {
+    "revision": "e0b8f7a79831216918c9",
+    "url": "main.css"
+  }
+];
