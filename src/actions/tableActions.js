9fefcde0d7da1e4764e7cac1b9b9ba7b1c877039
Victor Glindås <glinkis@gmail.com>
2018-12-14 16:42:39 +0100

    Revert invalid type.

--- src/actions/tableActions.js
+++ src/actions/tableActions.js
@@ -6,7 +6,7 @@ export const SORT_TABLE = "[table] SORT_TABLE";
 export const SET_TABLE_ROW = "[table] SET_TABLE_ROW";
 export const SET_TABLE_PAGE = "[table] SET_TABLE_PAGE";
 
-/** @type {Action<keyof import("../components/TableHeader").columns>} */
+/** @type {Action<string>} */
 export const sortTable = createAction(SORT_TABLE);
 
 /** @type {Action<number>} */
